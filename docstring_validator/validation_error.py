a797acdb2a49c2c4eafaed7665bc7d5b7d9c37f2
mtrzmiel <mtrzmiel@cisco.com>
2023-10-24 12:45:06 +0200

    Docstrings in validation_error module.

--- docstring_validator/validation_error.py
+++ docstring_validator/validation_error.py
@@ -1,4 +1,14 @@
+"""Library with classes for representing validation errors."""
+
+
 class ValidationError:
+    """Representation of single validation error.
+
+    Attributes:
+        code: error code describing validation error
+        text: description of discovered error
+    """
+
     def __init__(self, code: str, text: str):
         self.code = code
         self.text = text
